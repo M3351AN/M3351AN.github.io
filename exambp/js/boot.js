3491720eed91d8b2dded9bca2ad60613e52a1117
渟雲 <zuiun1337@gmail.com>
2024-01-11 12:03:28 +0800

    typo

--- exambp/js/boot.js
+++ exambp/js/boot.js
@@ -2,6 +2,6 @@ var bootMessageLines = [
             (new Date()).toString()+"<br>",
             "!!EDUCATIONAL PURPOSE ONLY!!<br>",
             "Click title above to view my GitHub or add my channel on title-bar<br>",
-            "Ingrese\"encrypt *pwd* 20221209\"para obtener la contraseña, o utilice el siguiente método <br>",
+            "Ingrese\"decrypt *pwd* 20221209\"para obtener la contraseña, o utilice el siguiente método <br>",
             "Reemplace\"exam.kernelsu.org\"en el enlace del que obtenga con\"tkm.icu/exambp/\"<br>"
         ];
