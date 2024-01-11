a65b0b0b1a22d1c133c5b0edfdb7b94dc0f7db95
渟雲 <zuiun1337@gmail.com>
2024-01-12 00:08:18 +0800

    打不过

--- exambp/js/boot.js
+++
@@ -1,7 +0,0 @@
-var bootMessageLines = [
-            (new Date()).toString()+"<br>",
-            "!!EDUCATIONAL PURPOSE ONLY!!<br>",
-            "Click title above to view my GitHub or add my channel on title-bar<br>",
-            "Ingrese\"decrypt *pwd* 20221209\"para obtener la contraseña, o utilice el siguiente método <br>",
-            "Reemplace\"exam.kernelsu.org\"en el enlace del que obtenga con\"tkm.icu/exambp/\"<br>"
-        ];
